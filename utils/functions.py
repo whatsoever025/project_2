ccb0bf890afd60b37f4fdeb8cf109c2aa58ca9d3
Trần Sỹ Minh Quân <minhquantsy@gmail.com>
2025-05-28 17:10:59 +0700

    Update functions.py

--- utils/functions.py
+++ utils/functions.py
@@ -160,9 +160,9 @@ def prepare_dataset(
     # Load dataset splits
     train_data, val_data, test_data = load_dataset_splits()
 
-    train_size = int(0.03 * len(train_data))  
-    val_size = int(0.03 * len(val_data))      
-    test_size = int(0.03 * len(test_data))    
+    train_size = int(0.02 * len(train_data))  
+    val_size = int(0.02 * len(val_data))      
+    test_size = int(0.02 * len(test_data))    
     train_indices = random.sample(range(len(train_data)), train_size)
     val_indices = random.sample(range(len(val_data)), val_size)
     test_indices = random.sample(range(len(test_data)), test_size)
